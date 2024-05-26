4bfee28a9776294257f247cc6fca381cc59c85fc
“DEBORAJ <deborajroy123@gmail.com>
2024-05-26 23:22:28 +0600

    Project Renamed

--- src/ECMS/ECMS.Web/Data/Entities/Discount.cs
+++ src/ECMS/ECMS.Web/Data/Entities/Discount.cs
@@ -4,6 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
         public decimal DiscountPercentage { get; set; }
 
         public ICollection<Order> Orders { get; set; }
