a6e62a6da9caca60d588ca382d2bd38f78549749
“DEBORAJ <deborajroy123@gmail.com>
2024-05-27 10:36:55 +0600

    Services class added

---
+++ src/ECMS/ECMS.Application/Services/IPostManagementService.cs
@@ -0,0 +1,16 @@
+﻿using ECMS.Domain.Entities;
+
+namespace ECMS.Application.Services
+{
+    public interface IProductManagementService
+    {
+        Task CreateProductAsync(string name, string description, decimal price, string imageUrl);
+        Task DeleteProductAsync(int id); 
+        Task<Product> GetProductAsync(int id);
+        Task UpdateProductAsync(int id, string name, string description, decimal price, string imageUrl);
+        Task<(IList<Product> records, int total, int totalDisplay)>
+            GetPagedProductsAsync(int pageIndex, int pageSize, string searchTitle,
+            string sortBy);
+        Task<IList<Product>>? GetProductAsync();
+    }
+}
---
+++ src/ECMS/ECMS.Application/Services/PostManagementService.cs
@@ -0,0 +1,62 @@
+﻿using ECMS.Domain.Entities;
+
+namespace ECMS.Application.Services
+{
+    public class ProductManagementService : IProductManagementService
+    {
+        private readonly IApplicationUnitOfWork _unitOfWork;
+
+        public ProductManagementService(IApplicationUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateProductAsync(string name, string description, decimal price, string imageUrl)
+        {
+            Product product = new Product
+            {
+                Name = name,
+                Description = description,
+                Price = price,
+                ImageUrl = imageUrl
+            };
+
+            await _unitOfWork.ProductRepository.AddAsync(product);
+            await _unitOfWork.SaveAsync(); 
+        }
+
+        public async Task<Product> GetProductAsync(int id)
+        {
+            return await _unitOfWork.ProductRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IList<Product>>? GetProductAsync()
+        {
+            return await _unitOfWork.ProductRepository.GetAllAsync();
+        }
+        public async Task DeleteProductAsync(int id)
+        {
+            await _unitOfWork.ProductRepository.RemoveAsync(id); 
+            await _unitOfWork.SaveAsync();
+        } 
+
+        public async Task UpdateProductAsync(int id, string name, string description, decimal price, string imageUrl)
+        {
+            var Product = await GetProductAsync(id);
+            if (Product is not null)
+            {
+                Product.Name = name;
+                Product.Description = description;
+                Product.Price = price;
+                Product.ImageUrl = imageUrl;
+            }
+
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task<(IList<Product> records, int total, int totalDisplay)> GetPagedProductsAsync(int pageIndex, int pageSize, string searchTitle, string sortBy)
+        {
+            return await _unitOfWork.ProductRepository.GetTableDataAsync(searchTitle, sortBy, pageIndex, pageSize);
+        }
+    }
+}
